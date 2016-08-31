e9091520970cec56c9787a55caaceb97fe55bf29
McKensie Mack <mckensiemack@gmail.com>
2016-08-30 21:56:16 -0500

    First commit on transform your life activity

--- src/transformYourLife.js
+++ src/transformYourLife.js
@@ -13,17 +13,47 @@ var loop = function(collection, callback){
 
 
 // 1. Build transform.
-
+var transform = function(collection, callback){
+	var results = [];
+	loop(collection, function(element) {
+	results.push(callback(element));
+	});
+	return results;
+}
 
 // 2. allNumbersMultipliedByThree
-var numbers = [1, 2, 3, 4, 5];
+	var numbers = [1, 2, 3, 4, 5];
+	var multipliedByThree  = transform(numbers, function(number){
+	return number *3;
+})
 
+//console.log(multipliedByThree);
 
 // 3. bestSentenceToUpperCase
-var bestSentence = "This is the best six week course ever!";
+	var bestSentence = "This is the best six week course ever!";
+	var bestSentenceToUpperCase = function(string){
+		var arrayOfWords = string.split(" ");
+		var newSentence = transform(arrayOfWords, function(word){
+			return word.toUpperCase();
+		})
+		return newSentence.join(" ");
+	}
+	console.log(bestSentenceToUpperCase("This is the best course."));
+	//transform(bestSentence, function(word){
+	//return bestSentence.toUpperCase();
+//});
+
 
 // 4. collectedContents
 var person = {name: 'Jon', greatestFear: 'fearItself'};
+var collectedContents = [];
+var collectedCharacteristics = transform(person, function(quality){
+for (key in person){
+	collectedContents.push(person);
+}
+});
+//console.log(collectedContents);
+//^^should this print more than once?
 
 // 5. multByThree
 
